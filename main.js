8b3b59e91e43814a83a1d1e44e7c8ae36264e606
boy1434@naver.com <boy1434@naver.com>
2024-07-01 01:05:07 +0900

    숫자맞추기 행맨 추가

--- main.js
+++ main.js
@@ -4,60 +4,71 @@ $(function() {
 });
 
 let comNum = 0;
-let chance = 5;
+let chance = 7;
+
 function randomNum() {
-    comNum = Math.floor(Math.random() * 100) +1;
+    comNum = Math.floor(Math.random() * 50) + 1;
     console.log(comNum);
 }
+
 function play() {
     let go_btn = $('.go_btn');
     let reset_btn = $('.reset_btn');
     let result = $('.result');
+    let hangmanParts = $('.hangman-part');
+    let crossLine = $('.cross_line');
 
     go_btn.on('click', function() {
         let userNum = parseInt($('.user_num').val());
-        if(isNaN(userNum)) {
-            result.text("유효한 숫자를 입력해주세요");
+        if (isNaN(userNum)) {
+            result.text("1부터 50 사이의 숫자를 입력해 주세요");
             result.css('color', 'red');
             return;
         }
-        if(userNum < comNum) {
-            result.text("랜덤 숫자는 더 높은 숫자 입니다.");
+        if (userNum < comNum) {
+            result.text("랜덤 숫자는 더 높은 숫자입니다.");
             result.css('color', 'red');
             chance--;
+            showHangMan(chance);
             $('.user_num').val('');
-        }
-        else if(userNum > comNum) {
-            result.text("랜덤 숫자는 더 낮은 숫자 입니다.");
+        } else if (userNum > comNum) {
+            result.text("랜덤 숫자는 더 낮은 숫자입니다.");
             result.css('color', 'red');
             chance--;
+            showHangMan(chance);
             $('.user_num').val('');
-        }
-        else if(userNum == comNum){
-            result.text("정답 입니다!");
+        } else if (userNum === comNum) {
+            result.text("정답입니다!");
             result.css('color', 'green');
             go_btn.prop('disabled', true);
             reset_btn.css('opacity', '1');
         }
         chanceUpdate();
 
-        if(chance <= 0) {
+        if (chance <= 0) {
             result.text("GAME OVER!");
             go_btn.prop('disabled', true);
             $('.chance').css('display', 'none');
             reset_btn.css('opacity', '1');
         }
     });
+
     reset_btn.on('click', function() {
         $(this).css('opacity', '0');
-        chance = 5;
+        chance = 7;
         chanceUpdate();
-        result.text("게임 시작!").css('color','black');
+        result.text("게임 시작!").css('color', 'black');
         $('.user_num').val('');
         go_btn.prop('disabled', false);
         randomNum();
+        hangmanParts.css('display', 'none');
     });
 }
+
 function chanceUpdate() {
     $('.chance').text(`Chance: ${chance}`);
+}
+
+function showHangMan(chance) {
+    $(`.hangman-part-${7 - chance}`).css('display', 'block');
 }
